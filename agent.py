d107bb5305bb400f06df06f5611b772695275cd3
stlalpha <jim.mcbride@gmail.com>
2023-03-16 10:14:32 -0500

    overhaul

--- agent.py
+++ agent.py
@@ -1,18 +1,32 @@
 import configparser
+import json
 import openai
 import os
 import sys
+import traceback
 from io import StringIO
 from contextlib import redirect_stdout, redirect_stderr
 from rich.console import Console
 from rich.syntax import Syntax
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-world_context_prompt = config.get('Prompts', 'world_context_prompt')
-loop_prompt_success = config.get('Prompts', 'loop_prompt_success')
-loop_prompt_error = config.get('Prompts', 'loop_prompt_error')
-num_iterations = config.getint('Settings', 'num_iterations')
+# Read configuration data from a JSON file
+def read_config(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            config = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print(f"Error: Unable to read or parse configuration file '{file_path}'.")
+        sys.exit(1)
+    
+    return config
+
+config_file_path = "config.json"
+config = read_config(config_file_path)
+
+world_context_prompt = config["Prompts"]["world_context_prompt"]
+loop_prompt_success = config["Prompts"]["loop_prompt_success"]
+loop_prompt_error = config["Prompts"]["loop_prompt_error"]
+num_iterations = config["Settings"]["num_iterations"]
 
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 
@@ -26,10 +40,12 @@ conversation_history = []
 
 console = Console()
 
+# Add message to conversation history
 def add_to_history(role, content):
     message = {"role": role, "content": content}
     conversation_history.append(message)
 
+# Ask DaVinci for a response based on the prompt and conversation history
 def ask_davinci(prompt, conversation_history):
     history = "".join([f"{msg['role']}: {msg['content']}\n" for msg in conversation_history])
     console.print(f"AI Prompt: {prompt}")
@@ -46,22 +62,41 @@ def ask_davinci(prompt, conversation_history):
     answer = response.choices[0].text.strip()
     return answer
 
+# Execute code and return output and success status
+import subprocess
+import tempfile
+
 def execute_code(code):
     console.print(f"Executing: {code}")
-    with StringIO() as stdout_buffer, StringIO() as stderr_buffer:
-        with redirect_stdout(stdout_buffer), redirect_stderr(stderr_buffer):
-            try:
-                exec(code)
-                stdout_output = stdout_buffer.getvalue()
-                stderr_output = stderr_buffer.getvalue()
-                success = True
-            except Exception as e:
-                stdout_output = stdout_buffer.getvalue()
-                stderr_output = f"Error: {str(e)}\n{traceback.format_exc()}"
-                success = False
+
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".py", delete=False) as temp_file:
+        temp_file.write(code)
+        temp_file.flush()
+
+    try:
+        result = subprocess.run(
+            ["python", temp_file.name],
+            text=True,
+            capture_output=True,
+            timeout=10  # Set a timeout to prevent long-running or infinite loops
+        )
+
+        stdout_output = result.stdout
+        stderr_output = result.stderr
+        success = result.returncode == 0
+
+    except subprocess.TimeoutExpired:
+        stdout_output = ""
+        stderr_output = "Error: Code execution timed out."
+        success = False
+
+    finally:
+        os.remove(temp_file.name)
 
     output = f"STDOUT:\n{stdout_output}\nSTDERR:\n{stderr_output}"
     return output, success
+
+# Main loop
 
 def main_loop(world_context_prompt, loop_prompt_success, loop_prompt_error, num_iterations):
     response = ask_davinci(world_context_prompt, conversation_history)
@@ -92,11 +127,9 @@ def main_loop(world_context_prompt, loop_prompt_success, loop_prompt_error, num_
         else:
             loop_prompt_error = loop_prompt_error.format(error="")
 
-
-
 if __name__ == "__main__":
     main_loop(world_context_prompt, loop_prompt_success, loop_prompt_error, num_iterations)
-    
+
     summary_prompt = config.get('Prompts', 'summary_prompt')
     summary_response = ask_davinci(summary_prompt, conversation_history)
     add_to_history("User", summary_prompt)
