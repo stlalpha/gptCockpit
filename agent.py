c253c05c054cc8cf190f99849d87c1b508d0d916
Jim McBride <jim.mcbride@gmail.com>
2023-03-16 13:57:19 -0500

    mostly works?

--- agent.py
+++ agent.py
@@ -63,7 +63,7 @@ def add_to_history(role, content):
 # Ask DaVinci for a response based on the prompt and conversation history
 def ask_davinci(prompt, conversation_history):
     history = "".join([f"{msg['role']}: {msg['content']}\n" for msg in conversation_history])
-    console.print(f"AI Prompt: {prompt}")
+    console.print(f"\nUser Prompt: {prompt}")
 
     response = openai.Completion.create(
         engine="text-davinci-003",
@@ -75,13 +75,15 @@ def ask_davinci(prompt, conversation_history):
     )
 
     answer = response.choices[0].text.strip()
+    print(f"Raw AI Response: {response.choices[0].text}")
+    print(f"Stripped AI Response: {answer}")
     return answer
+
 
 # Execute code and return output and success status
 
 def execute_code(code):
-    console.print(f"Executing: {code}")
-
+    print(f"Executing: {code}")
     with tempfile.NamedTemporaryFile(mode="w", suffix=".py", delete=False) as temp_file:
         temp_file.write(code)
         temp_file.flush()
@@ -117,6 +119,11 @@ def main_loop(world_context_prompt, loop_prompt_success, loop_prompt_error, num_
     for _ in range(num_iterations):
         current_prompt = context[-1]
         response = ask_davinci(current_prompt, conversation_history)
+
+        if response is None:
+            print("AI response is None. Skipping this iteration.")
+            continue
+
         add_to_history("User", current_prompt)
         add_to_history("AI", response)
         print("AI:", response)
@@ -151,16 +158,12 @@ if __name__ == "__main__":
     if use_default == "no" or use_default == "n":
         world_context_prompt = input("Please enter your custom world context prompt: ")
 
-    # Prompt for iteration count override
-    use_iteration_override = input("Do you want to override the default iteration count? (yes/no): ").lower()
-    if use_iteration_override == "yes" or use_iteration_override == "y":
+    override_iterations = input("Do you want to override the default iteration count? (yes/no): ").lower()
+    if override_iterations == "yes" or override_iterations == "y":
         num_iterations = int(input("Please enter the custom iteration count: "))
 
     main_loop(world_context_prompt, loop_prompt_success, loop_prompt_error, num_iterations)
     summary_prompt = config["Prompts"]["summary_prompt"]
     summary_response = ask_davinci(summary_prompt, conversation_history)
-    add_to_history("User", summary_prompt)
-    add_to_history("AI", summary_response)
-
-    console.print(f"User: {summary_prompt}\nAI: {summary_response}")
+    print(f"\nUser Prompt: {summary_prompt}\nAI Response: {summary_response}")
     sys.exit(0)
